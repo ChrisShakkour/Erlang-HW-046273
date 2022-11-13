e67070119e8dfe53b75c7fdb760b87636bef266d
Chris Shakkour <65361301+ChrisShakkour@users.noreply.github.com>
2022-11-13 12:12:57 +0200

    Update shapes.erl

---
+++ HW1/shapes.erl
@@ -0,0 +1,15 @@
+% Comments
+%
+
+% module decleration
+-module(shapes).
+
+
+% module export
+-export([shapesArea/1,
+         squaresArea/1,
+         trianglesArea/1,
+         shapesFilter/1,
+         shapesFilter2/1]).
+
+
